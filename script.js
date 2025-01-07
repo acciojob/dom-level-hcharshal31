071249bf6e55a6db6c79e23695fa209da04cf08b
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-07 18:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let targetElement = document.getElementById("level");
+
+function getLevel(element){
+	let level = 0;
+	while(element){
+		level++;
+		element = element.parentElement;
+	}
+	alert(`The level of the element is: ${level}`);
+}
+
+getLevel(targetElement);
